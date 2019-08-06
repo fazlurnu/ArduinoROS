e1f5e9d00db28a065ae4c8bfe7906a2b02dd57cc
fazlurnu <fazlur.rahman.ae@gmail.com>
2019-08-06 17:04:49 +0200

    Add state to Arduino code

--- ArduinoROS.ino
+++ ArduinoROS.ino
@@ -1,6 +1,7 @@
 #include <ros.h>
 #include <ros/time.h>
 #include <sensor_msgs/Range.h>
+#include <mavros_msgs/State.h>
 
 #include <NewPing.h>
 
@@ -8,16 +9,25 @@
 #define SONAR_NUM 2
 
 NewPing sonar[SONAR_NUM] = {   // Sensor object array.
-  NewPing(4, 5, MAX_DISTANCE), // Each sensor'se trigger pin, echo pin, and max distance to ping. 
-  NewPing(6, 7, MAX_DISTANCE)
+  NewPing(2, 3, MAX_DISTANCE), // Each sensor'se trigger pin, echo pin, and max distance to ping. 
+  NewPing(4, 5, MAX_DISTANCE)
 };
 
 ros::NodeHandle  nh;
 
+int value = 0;
+bool ascend = true;
+bool roverArmed = true;
+
+void stateCallback(const mavros_msgs::State& msg){
+  if(msg.armed)digitalWrite(13, HIGH);
+  else digitalWrite(13, LOW);
+}
+
 sensor_msgs::Range range_msg;
 ros::Publisher pub_range1( "/ultrasound1", &range_msg);
 ros::Publisher pub_range2( "/ultrasound2", &range_msg);
-
+ros::Subscriber<mavros_msgs::State> sub("/mavros/state", &stateCallback);
 char frameid[] = "/ultrasound";
 
 float getRange_Ultrasound(uint8_t index){
@@ -26,9 +36,12 @@ float getRange_Ultrasound(uint8_t index){
 
 void setup()
 {
+  pinMode(13, OUTPUT);
+  
   nh.initNode();
   nh.advertise(pub_range1);
   nh.advertise(pub_range2);
+  nh.subscribe(sub);
   
   range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
   range_msg.header.frame_id =  frameid;
@@ -38,13 +51,10 @@ void setup()
 }
 
 
-long range_time;
+long range_time = 0;
 
 void loop()
 {
-  
-  //publish the adc value every 50 milliseconds
-  //since it takes that long for the sensor to stablize
   if ( millis() >= range_time ){
     int r =0;
 
@@ -58,6 +68,6 @@ void loop()
     
     range_time =  millis() + 50;
   }
-  
+
   nh.spinOnce();
 }
